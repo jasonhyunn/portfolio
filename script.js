97773c8f34eecbc3b90fb52205cc4446c955d6f2
Unknown <jasonhyunn@gmail.com>
2019-01-16 01:29:40 -0800

    added modal

---
+++ script.js
@@ -0,0 +1,7 @@
+$('#contactModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget)
+    var recipient = button.data('email')
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+  });
