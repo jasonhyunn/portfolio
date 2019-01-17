ae62b55e18543e5cc30fad96d02c25f20ba219fd
Unknown <jasonhyunn@gmail.com>
2019-01-16 21:35:21 -0800

    edit modal / added tooltip / fully responsive

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ $('#contactModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget)
     var recipient = button.data('email')
     var modal = $(this)
-    modal.find('.modal-title').text('New message to ' + recipient)
-    modal.find('.modal-body input').val(recipient)
+    modal.find('.modal-title').text('Contact: ' + recipient)
+    // modal.find('.modal-body input').val(recipient)
   });
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
