05e16f0c0165b1dad105f20dbf19abfb3010ba6c
Unknown <jasonhyunn@gmail.com>
2019-01-22 11:17:17 -0800

    added more js/css animation.
    made comments to label where the animations are and organized the layout for easy readability.

--- script.js
+++ script.js
@@ -9,3 +9,52 @@ $('#contactModal').on('show.bs.modal', function (event) {
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  // $(function scrollReveal() {
+  //   window.sr = ScrollReveal();
+    
+  //   sr.reveal('.project-image', {
+  //     duration   : 800,
+  //     distance   : '20px',
+  //     easing     : 'ease-out',
+  //     origin     : 'bottom',
+  //     reset      : true,
+  //     scale      : 1,
+  //     viewFactor : 0,
+  //     afterReveal  : revealChildren,
+  //   }, 150);
+    
+  //     var revealChildren = sr.reveal('.project-text', {
+  //     duration   : 800,
+  //     scale      : 1,
+  //     distance   : '20px',
+  //     origin     : 'bottom',
+  //     reset      : true,
+  //     easing     : 'ease-out',
+  //     viewFactor : 1,
+  //   }, 75);
+  // })();
+
+//   $(document).ready(function() {
+    
+//     /* Every time the window is scrolled ... */
+//     $(window).scroll( function(){
+    
+//         /* Check the location of each desired element */
+//         $('.hideme').each( function(i){
+            
+//             var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//             var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+//             /* If the object is completely visible in the window, fade it it */
+//             if( bottom_of_window > bottom_of_object ){
+                
+//                 $(this).animate({'opacity':'1'},500);
+                    
+//             }
+            
+//         }); 
+    
+//     });
+    
+// });
